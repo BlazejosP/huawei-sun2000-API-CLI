83897bc67ab085eb8e05fc757ac5ceb450c26ab3
BlazejosP <60147797+BlazejosP@users.noreply.github.com>
2021-05-04 10:04:17 +0200

    Add files via upload
    First version with dialog G/UI

---
+++ fusionsolarapp_interface.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# possible arguments different gui systems
+# dialog
+# whiptail
+# gdialog
+# yad
+# Load configuration values variables
+#source config.conf
+source config.conf
+source functions.sh
+
+
+
+if [ -z "$userName" ] && [ -z "$systemCode" ];	
+	then	
+		login_text="username and password not exists in config.conf"
+	fi
+				
+if [ ! -z "$userName" ] && [ ! -z "$systemCode" ];	
+	then	
+		login_text="username and password exists in config.conf\n\n Username:$userName\n Password:$systemCode"
+		
+	fi
+
+
+DIALOG=${DIALOG:=dialog}
+
+
+	$DIALOG --title " Login to API" --clear \
+	--backtitle "Huawei FusionSolarApp API" \
+        --msgbox "$login_text" 10 50
+	--shadow
+clear #clears the terminal screen
+
+# Function to login to API
+login_to_API $DIALOG
+
+
+
+
+	$DIALOG --title " Login to API" --clear \
+	--backtitle $info_for_dialog_backtitle \
+        --msgbox  $info_for_dialog_screen 10 50
+      	--shadow
+clear #clears the terminal screen
