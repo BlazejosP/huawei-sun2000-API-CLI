89c2da687b8db15566239c4fb1b8fe3661fb6f26
BlazejosP <60147797+BlazejosP@users.noreply.github.com>
2021-05-24 23:36:26 +0200

    Add files via upload

--- fusionsolarapp.sh
+++ fusionsolarapp.sh
@@ -176,17 +176,17 @@ then
 			# Statistical data about whole Power Plant
 			
 			getStationRealKpi ${stations_Code_array[0]}	
-			#getKpiStationHour ${stations_Code_array[0]} $curent_time
-			#getKpiStationDay ${stations_Code_array[0]} $curent_time
-			#getKpiStationMonth ${stations_Code_array[0]} $curent_time
-			#getKpiStationYear ${stations_Code_array[0]} $curent_time
+			#getKpiStationHour ${stations_Code_array[0]} $date
+			#getKpiStationDay ${stations_Code_array[0]} $date
+			#getKpiStationMonth ${stations_Code_array[0]} $date
+			#getKpiStationYear ${stations_Code_array[0]} $date
 			
 			
 			# Statistical data about particular device/devices inside Power Plant
 			
 			# Devices data precisious all voltages etc real-time
 			getDevRealKpi  ${device_Id_array[0]} ${device_TypeId_array[0]}			
-			#getDevFiveMinutes ${device_Id_array[1]} ${device_TypeId_array[1]} $curent_time
+			#getDevFiveMinutes ${device_Id_array[1]} ${device_TypeId_array[1]} $date
 			#getDevKpiDay ${device_Id_array[1]} ${device_TypeId_array[1]} $curent_time
 			#getDevKpiMonth ${device_Id_array[1]} ${device_TypeId_array[1]} $curent_time
 			#getDevKpiYear ${device_Id_array[1]} ${device_TypeId_array[1]} $curent_time
@@ -195,7 +195,7 @@ then
 			#Error comunicates
 			
 			# we cover one month before chosen date that is as far as API allows
-			Begining_time=$(expr $curent_time - 2629743000)			
+			Begining_time=$(expr $date - 2629743000)			
 			#Languages. The value must be zh_CN, en_UK, ja_JP, it_IT, nl_NL, pt_BR, de_DE, fr_FR, es_ES, or po_PO.
 			language="en_UK"
 			#status Alarm status. Multiple alarm statuses are separated by commas (,), for example, 1,2. 1: not handled (active); 2: acknowledged (by a user); 3: being handled (transferred to a defect ticket); 4: handled (defect handling has ended); 5: cleared (by a user); 6: cleared (automatically by the device)
@@ -204,27 +204,13 @@ then
 			alarm_severity="1,2,3,4"
 			#Alarm type. Multiple alarm types are separated by commas (,), for example, 1,2. types 1: transposition signal; 2: exception alarm; 3: protection event; 4: notification status; 5: alarm information
 			alarm_type="1,2,3,4,5"
-			#Device type. Multiple device types are separated by commas (,), for example, 1,38. 1: Smart String Inverter; 2: SmartLogger; 8: transformer; 10: EMI; 13: protocol converter; 14: Central Inverter; 15: DC combiner box; 16: general device; 17: grid meter; 37: Pinnet data logger; 38: Smart Energy Center; 39: battery; 40: Smart Backup Box; 45: MBUS; 47: Power Sensor; 52: SAJ data logger; 53: high voltage bay of the main transformer; 54: main transformer; 55: low voltage bay of the main transformer; 56: bus bay; 57: line bay; 58: plant transformer bay; 59: SVC/SVG bay; 60: bus tie/section bay; 61: plant power supply device; 62: Dongle; 63: distributed SmartLogger; 70: safety box; 71: collector			
-			device_type="1,2,8,10,13,14,15,16,17,37,38,39,40,45,47,52,53,54,55,56,57,58,59,60,61,62,63,70,71"
+			#Device type. Multiple device types are separated by commas (,), for example, 1,38. 1: String Inverter; 2: Smart Logger; 8: transformer; 10: EMI; 13: protocol converter; 16: general device; 17: grid meter; 22: PID; 37: Pinnet data logger; 38: Residential inverter; 39: Battery; 40: Backup Box; 45: PLC; 47: Power Sensor; 62: Dongle; 63: distributed SmartLogger; 70: safety box;			
+			device_type="1,2,8,10,13,16,17,22,37,38,39,40,45,47,62,63,70"
 			
 			
-			#getAlarmList ${stations_Code_array[0]} $Begining_time $curent_time $language $status $alarm_severity $alarm_type $device_type
+			#getAlarmList ${stations_Code_array[0]} $Begining_time $date $language $status $alarm_severity $alarm_type $device_type
 			
 			
-			#Upgrade
-			
-			# Upgrade of firmware inside inverter or smart Energy Center 
-			#devUpgrade ${device_Id_array[1]} ${device_TypeId_array[1]}
-			
-			# info about upgrades and which were aplied when and what is their status
-			#getDevUpgradeInfo ${device_Id_array[1]} ${device_TypeId_array[1]}
-			
-			# this function is used to enter the email address or phone number 
-			# and the device SN to check whethever the SN has been registered 
-			# by the user unfortunetly can't made this working as for March 2021
-			# meaby still not implemented by Huwei or done something wrong
-			#
-			#snIsRegister ${device_esnCode_array[1]} $userName
 			
 
 			# Different dates for tests
@@ -253,6 +239,7 @@ then
 else
 	exit
 fi
+
 
 
 
